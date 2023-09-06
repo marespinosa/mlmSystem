2cd92cc567c03bde34f0583aebfd9d048fd502fa
@marespinosa <maricon@legendhasit.co.nz>
2023-09-06 13:25:21 +0800

    clean tree no show hide

--- resources/views/tree/tree-level1.blade.php
+++ resources/views/tree/tree-level1.blade.php
@@ -3,34 +3,6 @@ $counter = 0;
 @endphp
 
 
-<div class="treetabs">
-    <ul class="nav nav-tabs">
-        @if (isset($downlineUsers['level1']) && $downlineUsers['level1']->count() > 0)
-            @foreach ($downlineUsers['level1'] as $level1User)
-                @php
-                $counter++;
-                $tabId = 'tab' . $counter; // Increment the tab ID
-                $collapseId = 'collapse' . $counter; // Increment the collapse ID
-                @endphp
-
-                <li><a href="#{{ $tabId }}" data-toggle="tab">
-                        <div class="userTree">
-                            @if (auth()->user()->profile_picture)
-                                <img src="{{ asset(auth()->user()->profile_picture) }}" alt="Profile Picture" width="80" class="text-align">
-                            @else
-                                <img src="{{ asset('images/favicon.png') }}" alt="{{ $user->name }}" width="80" class="text-align"/>
-                            @endif
-                            {{ $level1User->name }} {{ $level1User->lastname }} <br />
-                            <b>Sponsor Id:</b> {{ $level1User->generatedId }}
-                        </div>
-                    </a></li>
-            @endforeach
-        @else
-            <p>No level 1 downline users found.</p>
-        @endif
-    </ul>
-
-    <div class="tab-content">
         @php
         $counter = 0; // Reset the counter
         @endphp
@@ -46,10 +18,7 @@ $counter = 0;
 
                 @endphp
 
-                <div class="tab-pane" id="{{ $tabId }}">
-                    <div class="panel panel-default">
-                        <div id="{{ $collapseId }}" class="panel-collapse">
-                            <div class="panel-body">
+              
                                 @if (isset($downlineUsers['level2'][$level1User->generatedId]) && count($downlineUsers['level2'][$level1User->generatedId]) > 0)
                 
                                 @php
@@ -90,16 +59,6 @@ $counter = 0;
                                 @else
                                 <p>No level 2 downline users found.</p>
                                 @endif
-
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
             @endforeach
         @endif
-             </div>
-        </div>
-    </div>
-</div>
-
+            
