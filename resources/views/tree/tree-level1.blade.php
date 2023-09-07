721eed094f7529f1ed64702f75b4f47c84a5d945
@marespinosa <maricon@legendhasit.co.nz>
2023-09-07 13:29:24 +0800

    working tree js

--- resources/views/tree/tree-level1.blade.php
+++ resources/views/tree/tree-level1.blade.php
@@ -2,63 +2,86 @@
 $counter = 0; 
 @endphp
 
-
-        @php
-        $counter = 0; // Reset the counter
-        @endphp
+<div class="easy-tree">
+    <ul>
         @if (isset($downlineUsers['level1']) && $downlineUsers['level1']->count() > 0)
             @foreach ($downlineUsers['level1'] as $level1User)
                 @php
                 $counter++;
                 $tabId = 'tab' . $counter; // Increment the tab ID
                 $collapseId = 'collapse' . $counter; // Increment the collapse ID
-                $toggleTab = 'toggleTab' . $counter; // Increment the collapse ID
-                $heading = 'heading' . $counter; // Increment the collapse ID
-
-
                 @endphp
 
-              
-                                @if (isset($downlineUsers['level2'][$level1User->generatedId]) && count($downlineUsers['level2'][$level1User->generatedId]) > 0)
+                  <li>
+                        <div class="userTree">
+                            @if (auth()->user()->profile_picture)
+                                <img src="{{ asset(auth()->user()->profile_picture) }}" alt="Profile Picture" width="80" class="text-align">
+                            @else
+                                <img src="{{ asset('images/favicon.png') }}" alt="{{ $user->name }}" width="80" class="text-align"/>
+                            @endif
+                            {{ $level1User->name }} {{ $level1User->lastname }} <br />
+                            <b>Sponsor Id:</b> {{ $level1User->generatedId }}
+                        </div>
                 
-                                @php
-                                $tabCounter = 0; 
-                                @endphp
-
-                                @foreach ($downlineUsers['level2'][$level1User->generatedId] as $level2User)
-                                
-                                @php
-                                    $tabCounter++;
-                                    $collapseId = 'collapse' . $tabCounter; 
-                                    $heading = 'heading' . $tabCounter; 
-                                @endphp
-                                                            
-                                <div class="level2-wrapper">
-                                    <div class="userTree level2" onclick="toggleContent({{ $tabCounter }})">
-
-                                    @if($level2User->profile_picture)
-                                        <img src="{{ asset($level2User->profile_picture) }}" alt="Profile Picture" width="80">
-                                    @else
-                                        <img src="{{ asset('images/favicon.png') }}" alt="{{ $level2User->name }}" width="80" />
-                                    @endif
-
-                                        <h6> {{ $level2User->name }} {{ $level2User->lastname }} </h6>
-                                            <b>Sponsor Id:</b> {{ $level2User->generatedId }}
-                                    </div>
-
-                                    @include('tree.tree-level3')
+                    <ul>
 
 
-                                </div>
+                    @if (isset($downlineUsers['level2'][$level1User->generatedId]) && count($downlineUsers['level2'][$level1User->generatedId]) > 0)
+        
+
+                    @php
+                    $tabCounter = 0;
+                    @endphp
+
+                    @foreach ($downlineUsers['level2'][$level1User->generatedId] as $level2User)
+                    
+                    @php
+                    $tabCounter++;
+                    $collapseId = 'collapse' . $tabCounter; // Increment the collapse ID
+                    $heading = 'heading' . $tabCounter; // Increment the collapse ID
+                    $level3 = 'level3' . $tabCounter; 
+
+                    @endphp
+                   
+                   <li>                             
+                      <div class="userTree level2">
+
+                        @if($level2User->profile_picture)
+                            <img src="{{ asset($level2User->profile_picture) }}" alt="Profile Picture" width="80">
+                        @else
+                            <img src="{{ asset('images/favicon.png') }}" alt="{{ $level2User->name }}" width="80" />
+                        @endif
+
+                            <h6> {{ $level2User->name }} {{ $level2User->lastname }} </h6>
+                            <b>Sponsor Id:</b> {{ $level2User->generatedId }}
 
 
-                                @endforeach
+                        </div>
 
-                               
+                        
+                        @include('tree.tree-level3')
 
-                                @else
-                                <p>No level 2 downline users found.</p>
-                                @endif
+                
+                </li>
+
+
+                    @endforeach
+
+
+                    </ul>
+                   
+
+                    @else
+                    <p>No level 2 downline users found.</p>
+                    @endif
+                
+
+                
+                </li>
             @endforeach
+        @else
+            <p>No level 1 downline users found.</p>
         @endif
-            
+    </ul>
+
+</div>
--- resources/views/tree/tree-level3.blade.php
+++ resources/views/tree/tree-level3.blade.php
@@ -1,7 +1,10 @@
-   @if (isset($downlineUsers['level3'][$level1User->generatedId][$level2User->generatedId]) && count($downlineUsers['level3'][$level1User->generatedId][$level2User->generatedId]) > 0) 
-        @foreach ($downlineUsers['level3'][$level1User->generatedId][$level2User->generatedId] as $level3User)
+<ul>
+@if (isset($downlineUsers['level3'][$level1User->generatedId][$level2User->generatedId]) && count($downlineUsers['level3'][$level1User->generatedId][$level2User->generatedId]) > 0) 
+    
+@foreach ($downlineUsers['level3'][$level1User->generatedId][$level2User->generatedId] as $level3User)
    
-       <div class="contentTab" style="display: none;">
+    <li>
+       <div class="userTree">
             @if($level3User->profile_picture)
                 <img src="{{ asset($level3User->profile_picture) }}" alt="Profile Picture" width="80">
             @else
@@ -12,9 +15,13 @@
         </div>
 
         @endforeach
-            @else
-                <p class="notfound contentTab" style="display: none;">No level 3 downline users found.</p>
-            @endif
+        
+        @else
+                <p class="notfound">No level 3 downline users found.</p>
+    
+        @endif
 
+    </li>
 
+</ul>
     
